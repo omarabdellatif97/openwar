d3dd8155f32de55e9b431b7332bcf900990212b8
unknown <omar_abdellatif97@hotmail.com>
2018-04-14 18:02:38 +0200

    another trial

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ app.get('/', function (req, res) {
     res.sendfile('./client/index.html');
 });
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 2000;
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 
