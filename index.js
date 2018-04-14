cff555d05d86585e86e98ff7b526ff1b45716a3c
unknown <omar_abdellatif97@hotmail.com>
2018-04-14 18:48:11 +0200

    another trial

--- index.js
+++ index.js
@@ -66,8 +66,8 @@ app.get('/', function (req, res) {
     res.sendfile('./client/index.html');
 });
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address =process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 
 http.listen(server_port,server_ip_address, function () {
