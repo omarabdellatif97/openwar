a58a8f8c11e147f400a4b5a9033c5f1ff40ef9e1
unknown <omar_abdellatif97@hotmail.com>
2018-04-22 14:27:46 +0200

    another trial

--- client/js/sketch.js
+++ client/js/sketch.js
@@ -3,6 +3,8 @@ var img;
 var tank={}, tank2;
 var tanks = [];
 var bullets = [];
+var dead = false;
+var fire = false;
 
 // var vertices = [[65, 40], [48, 40], [48, 41], [47, 41], [47, 42], [46, 42], [46, 43], [45, 43], [45, 44], [44, 44], [44, 45], [43, 45], [43, 46], [42, 46], [42, 47], [41, 47], [41, 48], [39, 48], [39, 49], [37, 49], [37, 50], [34, 50], [34, 51], [29, 51], [29, 52], [24, 52], [24, 51], [19, 51], [19, 50], [16, 50], [16, 49], [14, 49], [14, 48], [12, 48], [12, 47], [11, 47], [11, 46], [10, 46], [10, 45], [9, 45], [9, 44], [8, 44], [8, 43], [7, 43], [7, 42], [6, 42], [6, 41], [5, 41], [5, 39], [4, 39], [4, 38], [3, 38], [3, 35], [2, 35], [2, 32], [1, 32], [1, 20], [2, 20], [2, 17], [3, 17], [3, 15], [4, 15], [4, 13], [5, 13], [5, 11], [6, 11], [6, 10], [7, 10], [7, 9], [8, 9], [8, 8], [9, 8], [9, 7], [10, 7], [10, 6], [11, 6], [11, 5], [13, 5], [13, 4], [14, 4], [14, 3], [16, 3], [16, 2], [19, 2], [19, 1], [34, 1], [34, 2], [36, 2], [36, 3], [39, 3], [39, 4], [40, 4], [40, 5], [42, 5], [42, 6], [43, 6], [43, 7], [44, 7], [44, 8], [45, 8], [45, 9], [46, 9], [46, 10], [47, 10], [47, 11], [48, 11], [48, 12], [65, 12]];
 // var vertices = [[-50,-50], [50,-50], [50,50], [-50,50]];
@@ -25,12 +27,21 @@ function preload()
 function setup() {
     createCanvas(windowWidth, windowHeight);
     socket.on('tanks', function (data) {
-        tanks = data.tanks;
+        tanks = new Map(data.tanks);
         bullets = data.bullets;
-        var index = tanks.findIndex(findTank);
-        tank.x = tanks[index].x;
-        tank.y = tanks[index].y;
-		tanks.splice(index,1);
+        // var index = tanks.findIndex(findTank);
+        if(tanks.has(socket.id))
+        {
+            tank.x = tanks.get(socket.id).x;
+            tank.y = tanks.get(socket.id).y;
+            tank.health = tanks.get(socket.id).health;
+            tanks.delete(socket.id);
+        }
+        else
+        {
+            dead = true;
+        }
+        
     });
     imageMode(CENTER);
     rectMode(CENTER);
@@ -44,16 +55,15 @@ function fire() {
 function myshow(t) {
     push();
     translate(t.x, t.y);
-
-    var drawWidth = (t.health / 100) * 60;
+    // console.log(t.health);
+    var drawWidth = (t.health / 100) * 50;
     fill(0,0,0);
-    rect(0,-40,50,10);
+    rect(0,-40,drawWidth,10);
+    // stroke(255,0,0);
+    // noFill();
+    // rect(0,-40,50,10);
     rotate((t.angle));
     image(img, 0, 0);
-    // fill(0,255,0)
-    // ellipse(0,0,50,50);
-    // fill(255,0,128);
-    // rect(30,0,30,30);
     pop();
 }
 function myshow2(c) {
@@ -73,27 +83,44 @@ function update() {
     if (keyIsDown(68)) a++;
     socket.emit('move',{x:a,y:b,angle:angle});
 	tank.angle = angle;
-    if (keyIsDown(75)) {
+    if (keyIsDown(75)||fire===true) {
         console.log('fire');
         socket.emit('fire',{});
     }
 }
 function draw(){
-    if(tank!={})
+    // if(tank!={})
+    if(Object.keys(tank).length&&!dead)
     {
 
-        background(255);
+        background(222, 201, 255);
         translate(width / 2, height / 2);
         scale(width/1366);
         translate(-tank.x, -tank.y);
-        myshow(tank);
         update();
+        myshow(tank);
         for(var i=0;i<tanks.length;i++)
             myshow(tanks[i]);
+
+        for(let [pid, t] of tanks){
+                myshow(t);
+                // ctx.fillStyle = "purple";
+                // circle(player.x,player.y,10);
+            }
+
         for(var i=0;i<bullets.length;i++)
             myshow2(bullets[i]);
+    }
+    if(dead)
+    {
+        background(0);
     }
 }
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
+}
+function keyPressed() {
+  if (keyCode === 84) {
+    fire = !fire;
+  }
 }
